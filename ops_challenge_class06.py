e5f9a97b3414bf62bf03f65765a1217bf7b94ca1
drent23 <david.j.renteria1@gmail.com>
2024-01-16 17:16:09 -0600

    ops challenge class 06

---
+++ ops_challenge_class06.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+# Script Name: Encrypt/Decrypt File/Message
+# Author: David Renteria
+# Purpose: Get user to input target file or string to encrypt/decrypt
+
+# import Fernet & os modules
+from cryptography.fernet import Fernet
+import os
+
+# declare functions
+
+# generate key and save to file
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+    return key
+
+# loads key from current directory named 'key.key'
+def load_key():
+    return open("key.key", "rb").read()
+
+# mode 1, prompt user for target file path, encrypt file, delete existing version, and replace w/ encrypted version
+def enc_file(target_file):
+    key = load_key()
+    f = Fernet(key)
+
+    with open(target_file, "rb") as file:
+        dec_data = file.read()
+    enc_data = f.encrypt(dec_data)
+    os.remove(target_file)
+    with open(target_file, "wb") as file:
+        file.write(enc_data)
+
+# mode 2, prompt user for target file path, decrypt file, delete encrypted version, and replace w/ decrypted version
+def dec_file(target_file):
+    key = load_key()
+    f = Fernet(key)
+
+    with open(target_file, "rb") as file:
+        enc_data = file.read()
+    dec_data = f.decrypt(enc_data)
+    os.remove(target_file)
+    with open(target_file, "wb") as file:
+        file.write(dec_data)
+
+# mode 3, prompt user for PT string, encrypt, print/display CT string
+def enc_msg(msg):
+    key = load_key()
+    f = Fernet(key)
+    encrypted_msg = f.encrypt(msg.encode())
+    return encrypted_msg
+
+# mode 4, prompt user for CT string, decrypt, print/display PT string
+def dec_msg(msg):
+    key = load_key()
+    f = Fernet(key)
+    decrypted_msg = f.decrypt(msg)
+    return decrypted_msg.decode()
+
+# compress files
+def compress_file(target_file):
+    file_name = f"{target_file}.tar.gz"
+    os.system(f"tar -czf {file_name} {target_file}")
+    return file_name
+
+# main function
+def main():
+    user_mode = input("Please select whether you'd like to encrypt/decrypt a file or message: \n1 - Encrypt a file\n2 - Decrypt a file\n3 - Encrypt a message\n4 - Decrypt a message\n")
+    # conditionals based on user input
+    if user_mode == '1':
+        target_file = input("Please enter the path of your target file you want to encrypt: ")
+        enc_file(target_file)
+        if input("Would you like to compress the file as well? (Enter 'y' for yes or 'n' for no): ") == 'y':
+            file_compress = compress_file(target_file)
+            print(f"The file has been compressed and is now named {file_compress}")
+    elif user_mode == '2':
+        target_file = input("Please enter the path of your target file you want to decrypt: ")
+        dec_file(target_file)
+        if input("Would you like to compress the file as well? (Enter 'y' for yes or 'n' for no): ") == 'y':
+            file_compress = compress_file(target_file)
+            print(f"The file has been compressed and is now named {file_compress}")
+    elif user_mode == '3':
+        msg = input("Please enter a simple string you'd like to encrypt: ")
+        encrypted_msg = enc_msg(msg)
+        print("The encrypted string is as follows: ", encrypted_msg.decode())
+    elif user_mode == '4':
+        msg = input("Please enter the encrypted string you'd like to decrypt: ")
+        decrypted_msg = dec_msg(msg)
+        print("The decrypted string is as follows: ", decrypted_msg)
+    else:
+        print("Please only enter a number between 1 - 4.")
+
+if __name__ == "__main__":
+    main()
---
+++ python_notes.py
@@ -0,0 +1,73 @@
+# From Roger's class 6 lecture
+
+# Declare functions
+def encrypt(number, key):
+    encrypted_text = ""
+
+    for num in number:
+        numb = int(num, 16)
+        shifted_num = (numb + key) % 10
+        encrypted_text += str(shifted_num)
+    return encrypted_text
+
+def decrypt(encoded, key):
+    return encrypt(encoded, -key)
+
+if __name__ == "__main__":
+    encrypted = encrypt('123456', 3)
+    print(encrypted)
+    decrypted = decrypt(encrypted, 3)
+    print(decrypted)
+
+# must pip install cryptography package library then import Fernet module from it
+from cryptography.fernet import Fernet
+
+# generate key
+key = Fernet.generate_key()
+
+# assign key value to var
+f = Fernet(key)
+
+# convert PT to CT, 
+token = f.encrypt(b"encryption is fun")
+
+# 'b' = byte format, in this case binary...this can be removed using decode() method while printing original message (in libary's internal method)
+
+#  display CT
+print(token)
+
+# decrypt CT
+d = f.decrypt(token)
+
+# display PT
+print(d)
+
+# generate key and save to file
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+    return key
+
+# loads key from current directory named 'key.key'
+def load_key():
+    return open("key.key", "rb").read()
+
+# generate and write a new key
+write_key()
+
+# load previously generated key
+key = load_key()
+print("Key is " + str(key.decode('utf-8')))
+
+message = "hello friend".encode()
+print("PT is " + str(message.decode('utf-8')))
+
+# initialize Fernet class
+f = Fernet(key)
+
+# encrypt the message
+encrypted = f.encrypt(message)
+
+# print how it looks
+print("CT is " + encrypted.decode('utf-8'))
