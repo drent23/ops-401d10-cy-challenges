779467627c945b1ddee58aa23acb0cad79be0d78
drent23 <david.j.renteria1@gmail.com>
2024-01-22 15:55:38 -0600

    ops challenge 11 done

--- ops_challenge_class11.py
+++ ops_challenge_class11.py
@@ -6,24 +6,23 @@
 
 # import necessary modules
 import sys
-from scapy.all import Ether, IP, sniff, ARP, sr1, ICMP, TCP
+from scapy.all import Ether, IP, sniff, ARP, sr1, ICMP, TCP, send
 import random
 
 # assign target IP and port range to vars
 host = 'scanme.nmap.org'
-port_range = [20-23, 80, 443]
+port_range = [22, 80, 443]
 
 # define function to scan ports
 def scan_ports(dst_port):
     src_port = random.randint(1024, 65535)
-    response = sr1(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='5'), timeout=1, verbose=0)
+    response = sr1(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='S'), timeout=1, verbose=0)
     # conditionals to perform task based on port status
     if response is None:
         print(f"Port {dst_port} is filtered and silently dropped")
     elif response.haslayer(TCP):
         if response.getlayer(TCP).flags == 0x12:
-            rst_packet = sr1(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='5'), timeout=1, verbose=0)
-            send(rst_packet, verbose=0)
+            send(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='R'), verbose=0)
             print(f"Port {dst_port} is open")
         elif response.getlayer(TCP).flags == 0x14:
             print(f"Port {dst_port} is closed")
