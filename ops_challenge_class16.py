10fd6dc242ac27fc59a7bf8f11f021418a11b4af
drent23 <david.j.renteria1@gmail.com>
2024-01-29 17:01:23 -0600

    updated

--- ops_challenge_class16.py
+++ ops_challenge_class16.py
@@ -1,9 +1,14 @@
-# notes from demo/warmup time
+#!/usr/bin/env python3
 
-# import mods
-import nltk
+# Script Name: Brute Force Wordlist Attack Tool
+# Author: David Renteria
+# Purpose: Tool that uses both offensive & defensive dictionary attack tactics
+
+
+
+# import necessary modules
 import ssl
-from nltk.corpus import words
+import time
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -12,32 +17,45 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
-def get_words():
-    nltk.download('words')
-    word_list = words.words()
-    # print(word_list)
-    return word_list
+# define functions for modes
+def dict_it(target_file):
+    try:
+        # open file, read, and savie to mem as 'file'
+        with open(target_file, 'r') as file:
+            # iterate through word list
+            for word in file:
+                # remove any leading/ending characters
+                word = word.strip()
+                print(word)
+                # 2 second delay between words
+                time.sleep(2)
+    except FileNotFoundError:
+        print("Please check the file path and try again.")
 
-def check_words(words):
-    user_input = input("Please enter a word: ")
-    if user_input in words:
-        print("That word is indeed in the dictionary!")
-    else:
-        print("That word isn't in the dictionary.")
+def pass_rec(user_word, target_file):
+    try:
+        with open(target_file, 'r') as file:
+            if user_word in (word.strip() for word in file):
+                print(f"The word '{user_word}' is in the word list.")
+            else:
+                print(f"The word {user_word} is not in the word list.")
+    except FileNotFoundError:
+        print("That is not a valide file path.")
 
-def load_file():
-    password_list = []
-    with open('rockyou.txt', 'r') as file:
-        while True:
-            line = file.readline()
-            if not line:
-                break
-            line = line.rstrip()
-            password_list.append(line)
-            print(password_list)
+def main():
+    user_mode = input("Please enter '1' if you'd like to use the 'dictionay iterator' tactic or '2' if you want to check whether certain strings are in the password list\n")
+    if user_mode == '1':
+        target_file = input("Please enter the file path of the word list you want to search: ")
+        dict_it(target_file)
+    elif mode == '2':
+        user_word = input("Please enter a word you would like to search for in the password list: ")
+        target_file = input("Please enter the file path of the word list you want to search: ")
+        pass_rec(user_word, target_file)
+    else: 
+        print("Please only enter the number 1 or 2: ")
+if __name__ == "__main__":
+    main()
+
 
 if __name__ == "__main__":
-    ext_words = get_words()
-    # print(ext_words)
-    # check_words(ext_words)
-    load_file()
+    
--- python_notes.py
+++ python_notes.py
@@ -194,3 +194,46 @@ for host in ip_list:
     )
 # make sure not to ping broadcast or host IP
 
+# notes from demo/warmup time class 16
+
+# import mods
+import nltk
+import ssl
+from nltk.corpus import words
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+def get_words():
+    nltk.download('words')
+    word_list = words.words()
+    # print(word_list)
+    return word_list
+
+def check_words(words):
+    user_input = input("Please enter a word: ")
+    if user_input in words:
+        print("That word is indeed in the dictionary!")
+    else:
+        print("That word isn't in the dictionary.")
+
+def load_file():
+    password_list = []
+    with open('rockyou.txt', 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            line = line.rstrip()
+            password_list.append(line)
+            print(password_list)
+
+if __name__ == "__main__":
+    ext_words = get_words()
+    # print(ext_words)
+    # check_words(ext_words)
+    load_file()
