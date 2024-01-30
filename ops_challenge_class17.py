255a26a8dfd665acd0115098ff7847c7f2997267
drent23 <david.j.renteria1@gmail.com>
2024-01-30 17:14:22 -0600

    still a work in progress

--- ops_challenge_class17.py
+++ ops_challenge_class17.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
-# Script Name: Brute Force Wordlist Attack Tool
+# Script Name: Brute Force Wordlist Attack Tool Pt 2
 # Author: David Renteria
-# Purpose: Tool that uses both offensive & defensive dictionary attack tactics
+# Purpose: Tool that uses both offensive & defensive dictionary attack tactics by authenticating to an SSH server by its IP address
 
 
 
 # import necessary modules
 import ssl
 import time
+import paramiko
+import getpass
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -42,26 +44,6 @@ def pass_rec(user_word, target_file):
     except FileNotFoundError:
         print("That is not a valide file path.")
 
-def main():
-    user_mode = input("Please enter '1' if you'd like to use the 'dictionay iterator' tactic or '2' if you want to check whether certain strings are in the password list\n")
-    if user_mode == '1':
-        target_file = input("Please enter the file path of the word list you want to search: ")
-        dict_it(target_file)
-    elif user_mode == '2':
-        user_word = input("Please enter a word you would like to search for in the password list: ")
-        target_file = input("Please enter the file path of the word list you want to search: ")
-        pass_rec(user_word, target_file)
-    else: 
-        print("Please only enter the number 1 or 2: ")
-
-if __name__ == "__main__":
-    main()
-
-    # Roger's class17 demo
-
-    import paramiko
-    imnport getpass
-
 def get_host():
     host = input("Enger an SSH Client to connect to or enter for default: ") or "192.168.12.211"
     return host
@@ -74,7 +56,7 @@ def get_password():
     password = input("Please enter a password or enter for default: ") or "password1234"
     return password
 
-def ssh_client():
+def ssh_endpoint():
     port = 22
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -98,6 +80,19 @@ def ssh_client():
         print("Authentication failed.")
         print(e)
 
+def main():
+    user_mode = input("Please enter '1' if you'd like to use the 'dictionay iterator' tactic or '2' if you want to check whether certain strings are in the password list\n")
+    if user_mode == '1':
+        target_file = input("Please enter the file path of the word list you want to search: ")
+        dict_it(target_file)
+    elif user_mode == '2':
+        user_word = input("Please enter a word you would like to search for in the password list: ")
+        target_file = input("Please enter the file path of the word list you want to search: ")
+        pass_rec(user_word, target_file)
+    else: 
+        print("Please only enter the number 1 or 2: ")
+
 if __name__ == "__main__":
-    ext_words = get_words()
-    ssh_client()
+    main()
+    ssh_endpoint()
+    # Roger's class17 demo
