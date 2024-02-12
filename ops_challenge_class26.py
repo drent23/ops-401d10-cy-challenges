89dc7674cb7ef0b4077ec15c01385ea4b2f5207e
drent23 <david.j.renteria1@gmail.com>
2024-02-12 15:27:56 -0600

    I think I'm done....

--- ops_challenge_class26.py
+++ ops_challenge_class26.py
@@ -9,47 +9,43 @@ import logging
 from scapy.all import IP, sr1,TCP, send
 import random
 
-# assign target IP and port range to vars
+# Assign target IP and port range to vars
 host = 'scanme.nmap.org'
 port_range = [20, 21, 22, 23, 53, 69, 80, 123, 179, 264, 443, 520]
 
-# define function to scan ports
+# Define a port scanning function
 def scan_ports(dst_port):
     src_port = random.randint(1024, 65535)
     response = sr1(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='S'), timeout=1, verbose=0)
     # conditionals to perform task based on port status
     if response is None:
-        print(f"Port {dst_port} is filtered and silently dropped")
+        result = f"Port {dst_port} is filtered and silently dropped"
     elif response.haslayer(TCP):
         if response.getlayer(TCP).flags == 0x12:
             send(IP(dst=host)/TCP(sport=src_port, dport=dst_port, flags='R'), verbose=0)
-            print(f"Port {dst_port} is open")
+            result = f"Port {dst_port} is open"
         elif response.getlayer(TCP).flags == 0x14:
-            print(f"Port {dst_port} is closed")
+            result = f"Port {dst_port} is closed"
+    return result
 
-# Define a function
-def log_job():
-    # Create log object
-    log = logging.getLogger("data_logger")
+# Create log object
+log = logging.getLogger("data_logger")
 
-    # Config logging object
-    logging.basicConfig(filename='bruteforce.log',level=logging.DEBUG, filemode='w', format='%(asctime)s - %(levelname)s -%(message)s')
+# Config logging object
+logging.basicConfig(filename='bruteforce.log',level=logging.DEBUG, filemode='w', format='%(asctime)s - %(levelname)s -%(message)s')
 
-    try: 
-        # create for loop to scan each port within the port range
-        for port in port_range:
-            scan_ports(port)
-            log.info(f"The results of the port scan are now logged as {scan_ports}")
-    except Exception as e:
-        log.error(f"We ran into the following error: {e}")
-    try:
-        for port in port_range:
-            scan_ports(port)
-            log.info(f"The results of the port scan are now logged as {scan_ports}")
-            log.warning("This is a warning")
-            log.critical("This is critical")
-    except FileNotFoundError as e:
-        log.exception("There was an error locating that file")
+# Define a logging function
+def log_job(host, port_range):
+    # create for loop to scan each port within the port range
+    for port in port_range:
+        try:
+            result = scan_ports(host, port_range)
+            log.info(result)
+            log.debug(result)
+            log.warning(result)
+            log.critical(result)
+        except Exception as e:
+            log.error(f"We ran into the following error at port {port}: {e}")
 
 # Call function
-log_job()   
+log_job(host, port_range)
