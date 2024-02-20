6bad0930606d9dfc5e9ccb1fc453d6f6c6005d66
drent23 <david.j.renteria1@gmail.com>
2024-02-20 15:54:05 -0600

    challenge class31 done

--- ops_challenge_class31.py
+++ ops_challenge_class31.py
@@ -1,46 +1,41 @@
 #!/usr/bin/env python3
 
-# Script Name: Port Scanner with Rotating Log based on Size & Time and both File & Stream Handlers
+# Script Name: Sig-based Malware detection pt 1
 # Author: David Renteria
-# Purpose: Tool that uses scapy to scan ports then rotates logs based on time and size and outputs File Handlers to log files and Stream Handlers to display to screen
+# Purpose: Basic Python AV tool
 
 # import modules
 from sys import platform
-import os, time
+import os, time, logging
+
+# configure logs
+logging.basicConfig(filename='malware_threat.log',level=logging.INFO, filemode='w', format='%(asctime)s - %(levelname)s -%(message)s')
 
 # declare functions
 
-# Function that handles Linux search
-# def linux_search():
-#     whichFile = input("What file are you searching for?")
-#     directory = input("Which directory do you want to search in?")
-#     os.system("Find " + str(directory) + " | echo 'Searched $(wc -l) files.\'")
-#       call hash_function in here
-# Function that handles Windows search
-# def win_search():
-#       call hash_function in here
-# def hash_function 
-def hash_file(target_file):
-    # create hash object
-    hash = hashlib.sha256()
+# Function that handles search for both Linux & Windows using os.walk & os.path feature
+def file_search(target_file, directory):
+    searched_files = 0
+    hits_found = 0
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            searched_files += 1
+            if file == target_file:
+                hits_found += 1
+                path = os.path.join(root, file)
+                print(f"The path to the file you requested is as follows: {path}")
+    print(f"The number of files that were searched is: {searched_files}")
+    print(f"The number of hits found is: {hits_found}")
+    logging.info(f"# of searched files: {searched_files}; Hits found: {hits_found}; Target file path: {path}")
 
-    # open file for reading in binary mode
-    with open(target_file, 'rb') as file:
-        # loop through end of file
-        chunk = 0
-        while chunk != b'':
-            # read 1024 bytes at a time
-            chunk = file.read(1024)
-            print(chunk)
-            hash.update(chunk)
-    # return hex represenation of hash
-    return hash.hexdigest()
+# prompt user for target file & directory and search system directory
+target_file = input("Please enter the filename are you searching for: ")
+directory = input("Please enter the directory you want to search in: ")
 
-# OS system check
+# OS system check & call function
 if platform == "linux" or platform == "linux2":
     print("This is a Linux machine")
-    # linux_search()
+    file_search(target_file, directory)
 elif platform == "win32":
     print("This is a Windows machine")
-    # win_search()
-
+    file_search(target_file, directory)
